944862b8f8f943fc4d350c7d6e4f00e1caa39110
chenjiahui <chenjiahui_job@163.com>
2018-01-16 20:00:21 +0800

    20180116commit

---
+++ src/main/java/com/shang9/magiccook/MagiccookApplication.java
@@ -0,0 +1,12 @@
+package com.shang9.magiccook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagiccookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagiccookApplication.class, args);
+	}
+}
